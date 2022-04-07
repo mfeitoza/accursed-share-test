6c09589038d80bc88069ae9906e3215ba9aa9b83
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-04-07 16:09:12 -0300

    refactor(App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "App.css";
+
+import Chip from "./components/chip";
+import Card from "./components/card";
+import Dropdown from "./components/dropdown";
+import Tooltip from "./components/ballon";
 
 function App() {
+  const avatar =
+    "https://images.unsplash.com/photo-1612896488082-7271dc0ed30c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80";
+  const image =
+    "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80";
+  const options = [
+    { label: 'Option 1', value: 1 },
+    { label: 'Option 2', value: 2 },
+    { label: 'Option 3', value: 3 },
+    { label: 'Option 4', value: 4 },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Chip name="Trixie" avatar={avatar} />
+      <br />
+      <br />
+      <div style={{ display: 'flex' }}>
+        <div style={{ marginRight: '1rem'}}><Card image={image} title="Lorem ipsum" text="Dui ut ornare lectus sit amet est placerat. Risus nec feugiat in fermentum posuere urna nec. Interdum velit euismod in pellentesque massa placerat duis." /></div>
+        <div style={{ marginLeft: '1rem'}}><Card image={image} title="Lorem ipsum" text="Dui ut ornare lectus sit amet est placerat. Risus nec feugiat in fermentum posuere urna nec. Interdum velit euismod in pellentesque massa placerat duis." /></div>
+      </div>
+      <br />
+      <br />
+      <Dropdown name="selectOne" label="Select" options={options} placeholder="Select one option" />
+      <br />
+      <br />
+      <Tooltip text="Lorem ipsum dollor">
+        <div style={{ padding: '20px', background: '#eee' }}>Tincidunt ornare massa eget</div>
+      </Tooltip>
     </div>
   );
 }
