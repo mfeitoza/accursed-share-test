0733e5b02360ccb7a7dac7cb8e1f681cd9190443
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-04-07 16:08:50 -0300

    feat(components): add arrow to ballon

--- src/components/ballon/style.ts
+++ src/components/ballon/style.ts
@@ -1,4 +1,4 @@
-import styled, {  } from "styled-components";
+import styled from "styled-components";
 
 type TooltipProps = {
   wrapperWidth: number;
@@ -33,23 +33,61 @@ export const Tooltip = styled.div<TooltipProps>`
         return `
           top: ${-(props.tooltipHeight + 10)}px;
           left: ${(props.wrapperWidth - props.tooltipWidth) / 2}px;
-        `
+        `;
       case "down":
         return `
           bottom: ${-(props.tooltipHeight + 10)}px;
           left: ${(props.wrapperWidth - props.tooltipWidth) / 2}px;
-        `
+        `;
       case "left":
         return `
           top: ${(props.wrapperHeight - props.tooltipHeight) / 2}px;
           left: ${-(props.tooltipWidth + 10)}px;
-        `
+        `;
       case "right":
         return `
           top: ${(props.wrapperHeight - props.tooltipHeight) / 2}px;
-          left: ${(props.wrapperWidth + 10)}px;
-        `
+          left: ${props.wrapperWidth + 10}px;
+        `;
     }
   }}
-  
+
+  &::after {
+    content: "";
+    position: absolute;
+    border-width: 5px;
+    border-style: solid;
+    ${(props) => {
+      switch (props.direction) {
+        case "up":
+          return `
+            top: 100%;
+            left: 50%;
+            margin-left: -5px;
+            border-color: rgba(0, 0, 0, 0.7) transparent transparent transparent;
+          `;
+        case "down":
+          return `
+            bottom: 100%;
+            left: 50%;
+            margin-left: -5px;
+            border-color: transparent transparent rgba(0, 0, 0, 0.7) transparent;
+          `;
+        case "left":
+          return `
+            top: 50%;
+            left: 100%;
+            margin-top: -5px;
+            border-color: transparent transparent transparent rgba(0, 0, 0, 0.7);
+          `;
+        case "right":
+          return `
+            top: 50%;
+            right: 100%;
+            margin-top: -5px;
+            border-color: transparent rgba(0, 0, 0, 0.7) transparent transparent;
+          `;
+      }
+    }}
+  }
 `;
